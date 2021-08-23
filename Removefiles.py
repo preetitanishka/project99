5aa3cd93af2422e1905e0d32674786892db8eff3
preetitanishka <71656803+preetitanishka@users.noreply.github.com>
2021-08-23 17:51:56 +0530

    Add files via upload

---
+++ Removefiles.py
@@ -0,0 +1,13 @@
+import os
+import time
+import shutil
+
+path="C:/Users/Admin/Desktop/trialProject99"
+time.time()
+os.path.exist(C:/Users/Admin/Desktop/project99/trialProject99)
+os.walk(C:/Users/Admin/Desktop/project99/trialProject99)
+os.path.join()
+os.stat(C:/Users/Admin/Desktop/project99/trialProject99)
+ctime=os.stat(C:/Users/Admin/Desktop/project99/trialProject99).st_ctime
+os.remove(C:/Users/Admin/Desktop/project99/trialProject99)
+os.rmtree(C:/Users/Admin/Desktop/project99/trialProject99)
